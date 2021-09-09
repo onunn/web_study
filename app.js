4e173c28aa94a3892d08954c8b22de7b5d22c9d7
mingyu_choi <ligyu3465@gmail.com>
2021-09-09 19:57:06 +0900

    change app.js
    study about localStorage

--- app.js
+++ app.js
@@ -9,21 +9,36 @@ or
 const loginForm = document.querySelector('.login-form');
 const loginInput = document.querySelector('.login-form input');
 const loginButton = document.querySelector('.login-form button');
+const greeting = document.querySelector('.greeting');
 
 const link = document.querySelector('a');
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY =    'username';
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
 
-    console.log(username)
-    
+    localStorage.setItem(USERNAME_KEY, username);
+
+    //greeting.innerText = 'Hello ' + username;
+    paintGreetings(username)
 }
 
-function handleLinkClick(event){
-    event.preventDefault();
-    console.dir(event);
-    alert('clicked');
-}
+
 loginForm.addEventListener('submit', onLoginSubmit);
-link.addEventListener('click', handleLinkClick);
+
+function paintGreetings(username){
+    //const username = localStorage.getItem(USERNAME_KEY)
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+    paintGreetings(savedUsername);
+}
